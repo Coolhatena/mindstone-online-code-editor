0696581d9b8cc4407b5afc365fce186b5080674b
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-02-22 17:32:55 -0800

    Add new editor input

--- src/app/components/OperationsInput.jsx
+++ src/app/components/OperationsInput.jsx
@@ -1,6 +1,9 @@
 'use client'
 import analizer from '@/module/analizer';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 
 const OperationsInput = () => {
 	const [inputText, setInputText] = useState("");
@@ -11,10 +14,15 @@ const OperationsInput = () => {
 										})
 	const [errors, setErrors] = useState([])
 
-
-	const onTextareaChange = (event) => {
-		setInputText(event.target.value);
-	};
+	useEffect(() => {
+		const delayDebounceFn = setTimeout(() => {
+			const [res, err] = analizer(inputText);
+			setResults(res);
+			setErrors(err);
+		}, 1000)
+	
+		return () => clearTimeout(delayDebounceFn)
+	  }, [inputText])
 
 	let handleResolveClick = () => {
 		const [res, err] = analizer(inputText)
@@ -28,56 +36,55 @@ const OperationsInput = () => {
 	}
 
 	return (
-		<div className='operations-input'>
-			<p className='operations-input-title'>Test</p>
-			<textarea
-				id="operation-textarea"
-				className='operations-input-textarea'
-				value={inputText}
-				onChange={onTextareaChange}
-				placeholder='Ingrese sus operaciones'
-			></textarea>
-			<div className='button-container'>
-				<button id="resolve" onClick={handleResolveClick} className='operations-input-button'>
-					Resolver
-				</button>
-				<button id="resolve" onClick={clean} className='operations-input-button'>
-					Limpiar
-				</button>
-			</div>
-			{
-				errors.length ?
-				<>
+		<>
+			<CodeMirror
+					value={inputText}
+					onChange={(e) => setInputText(e)}
+					height="20rem"
+					width='100%'
+					theme={vscodeDark}
+				/>
+			<div className='operations-input'>
+				
+				<div className='button-container'>
+					<button id="resolve" onClick={clean} className='operations-input-button'>
+						Limpiar
+					</button>
+				</div>
 				{
-					errors.map((text, i) => (
-						<p key={i}>ERROR: {text}</p>
-					))
-				}
-				</> 
-					:
-					null
-			}
-			{
-				(results.ent.length && !errors.length) ? 
-					<p>ent: {
-					results.ent.map( (obj) => obj.id ).join(', ')}</p>
-					:
-					null
-			}		
-			{
-				(results.pdec.length && !errors.length) ? 
+					errors.length ?
+					<>
+					{
+						errors.map((text, i) => (
+							<p key={i}>ERROR: {text}</p>
+							))
+						}
+					</> 
+						:
+						null
+					}
+				{
+					(results.ent.length && !errors.length) ? 
+						<p>ent: {
+							results.ent.map( (obj) => obj.id ).join(', ')}</p>
+							:
+							null
+						}		
+				{
+					(results.pdec.length && !errors.length) ? 
 					<p>pdec: {results.pdec.map( (obj) => obj.id ).join(', ')}</p>
 					:
 					null
-			}	
-			{
-				(results.ctr.length && !errors.length) ? 
+				}	
+				{
+					(results.ctr.length && !errors.length) ? 
 					<p>ctr: {results.ctr.map( (obj) => obj.id ).join(', ')}</p>
-					:
-					null
-			}	
+						:
+						null
+				}	
 
-		</div>
+			</div>
+		</>
 	);
 };
 
