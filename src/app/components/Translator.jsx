9f23c8ab335d119e094bcd22d4abb57b09a5ade8
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-05-17 21:51:36 -0700

    Separate code editors as a independent component

--- src/app/components/Translator.jsx
+++ src/app/components/Translator.jsx
@@ -5,86 +5,51 @@ import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 
 import CtoMSTranslator from '@/module/CtoMSTranslator';
+import DualSwitchButton from './DualSwitchButton';
+import CodeEditor from './codeEditor';
 
 const Translator = ({ codeState=["", () => {}] }) => {
-	const [inputText, setInputText] = codeState;
-	const [inputTextCLang, setInputTextCLang] = useState("");
+	const [switchTranslation, setSwitchTranslation] = useState(true);
+	const [mindstoneCode, setMindstoneCode] = codeState;
+	const [cCode, setCCode] = useState("");
+	const [jazminCode, setJazminCode] = useState("")
 	const inputFile = useRef(null);
 
 	let handleTranslateCtoMS = () => {
-		let result = CtoMSTranslator(inputTextCLang)
-		setInputText(result)
-	};
-
-	let handleFileLoad = (e) => {
-		e.preventDefault()
-		const reader = new FileReader();
-		if (e.target.files.length){
-			reader.readAsText(e.target.files[0]);
-			reader.onload = (e) => {
-				const file = e.target.result;
-				setInputTextCLang(file);
-			}
-		}
-	};
-
-	let handleClickFileLoad = () => {
-		inputFile.current.click();
-		inputFile.current.value = ""
-	}
-
-	let HandleCleanMs = () => {
-		setInputText("");
-	};
-
-	let HandleCleanC = () => {
-		setInputTextCLang("");
+		let result = CtoMSTranslator(cCode)
+		setMindstoneCode(result)
 	};
 
 	return (
-		<div className='translator'>
-			<div className="container-translator">
-				<div className="editor">
-					<div className="editor-header">
-						<h2>C Code</h2>
-						<div className="button-container">
-							<button id="load-file-button" className="editor-button" onClick={handleClickFileLoad}>
-								Load file
-								<input type='file' name='load-file' id="load-file" accept=".txt, .c" ref={inputFile} onChange={(e) => handleFileLoad(e)} className="file-reader" />	
-							</button>
-							
-							<button id="translateC" onClick={handleTranslateCtoMS} className="editor-button">Translate</button>
-							<button id="clear" onClick={HandleCleanC} className="editor-button">Clear</button>
-						</div>
-					</div>
-					<CodeMirror
-						value={inputTextCLang}
-						onChange={(e) => setInputTextCLang(e)}
-						height="20rem"
-						width="100%"
-						theme={vscodeDark}
-					/>
-				</div>
+		<div>
+			<div className='translator-switch'>
+				<DualSwitchButton leftOption="C to MindStone" rightOption="MindStone to Jazmin" switchVariable={switchTranslation} setSwitchVariable={setSwitchTranslation}/>
 			</div>
+			{
+				switchTranslation ?
+				// C to MindStone 
+				<div className='translator'>
+					<div className="container-translator">
+						<CodeEditor title="C code" textVariable={cCode} setTextVariable={setCCode} isTranslateOption handleTranslate={handleTranslateCtoMS} isLoadFileOption acceptedFileTypes='.txt, .c'/>
+					</div>
 
-			<div className="container-translator">
-				<div className="editor">
-					<div className="editor-header">
-						<h2>MindStone Code</h2>
-						<div className="button-container">
-							<button id="translateMS" onClick={handleTranslateCtoMS} className="editor-button">Translate</button>
-							<button id="clear" onClick={HandleCleanMs} className="editor-button">Clear</button>
-						</div>
+					<div className="container-translator">
+						<CodeEditor title="MindStone code" textVariable={mindstoneCode} setTextVariable={setMindstoneCode}/>
 					</div>
-					<CodeMirror
-						value={inputText}
-						onChange={(e) => setInputText(e)}
-						height="20rem"
-						width="100%"
-						theme={vscodeDark}
-					/>
+				</div> 
+				: 
+				// MindStone to Jazmin
+				<div className='translator'>
+					<div className="container-translator">
+						<CodeEditor title="MindStone code" textVariable={mindstoneCode} setTextVariable={setMindstoneCode} isTranslateOption handleTranslate={handleTranslateCtoMS}/>
+					</div>
+
+					<div className="container-translator">
+						<CodeEditor title="Jazmin code" textVariable={jazminCode} setTextVariable={setJazminCode}/>
+					</div>
 				</div>
-			</div>
+			}
+			
 		</div>
 	);
 }
---
+++ src/app/components/codeEditor.jsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from 'react'
+
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+
+const CodeEditor = ({title, textVariable, setTextVariable = () => {}, isTranslateOption, handleTranslate = () => {}, isLoadFileOption = false, acceptedFileTypes = '.txt'}) => {
+	const inputFile = useRef(null);
+
+	let handleFileLoad = (e) => {
+		e.preventDefault()
+		const reader = new FileReader();
+		if (e.target.files.length){
+			reader.readAsText(e.target.files[0]);
+			reader.onload = (e) => {
+				const file = e.target.result;
+				setTextVariable(file);
+			}
+		}
+	};
+
+	let handleClickFileLoad = () => {
+		inputFile.current.click();
+		inputFile.current.value = ""
+	}
+
+  return (
+	<div className="editor">
+		<div className="editor-header">
+			<h2>{title}</h2>
+			<div className="button-container">
+				{
+					isLoadFileOption ?
+						<button id="load-file-button" className="editor-button" onClick={handleClickFileLoad}>
+							Load file
+							<input type='file' name='load-file' id="load-file" accept={acceptedFileTypes} ref={inputFile} onChange={(e) => handleFileLoad(e)} className="file-reader" />	
+						</button>
+					:
+						<></>
+				}
+				{
+					isTranslateOption ?
+						<button id="translate" onClick={handleTranslate} className="editor-button">Translate</button>
+					:
+						<></>
+
+				}
+				<button id="clear" onClick={() => setTextVariable("")} className="editor-button">Clear</button>
+			</div>
+		</div>
+		<CodeMirror
+			value={textVariable}
+			onChange={(e) => setTextVariable(e)}
+			height="20rem"
+			width="100%"
+			theme={vscodeDark}
+		/>
+	</div>
+  )
+}
+
+export default CodeEditor
